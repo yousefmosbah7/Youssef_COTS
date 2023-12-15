73d62f6b531e55968bb847d34f270b756c10756e
Youssef_Mosbah <yousefmosbah7@gmail.com>
2023-12-15 10:58:28 +0200

    Remapping Function

---
+++ Library/REMAPPING.h
@@ -0,0 +1,6 @@
+#ifndef REMAPPING_H
+#define REMAPPING_H
+
+sint32 MAP(sint32 Copy_s32InputMin,sint32 Copy_s32InputMax,sint32 Copy_s32OutputMin,sint32 Copy_s32OutputMax,sint32 Copy_s32InputVal);
+
+#endif
